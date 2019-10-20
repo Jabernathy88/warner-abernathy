b414f77961d4c9320279217c449021c494029af3
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-10-20 18:03:44 -0400

    Successfull load of Mongo collections in Express.

--- api/server.js
+++ api/server.js
@@ -1,26 +1,38 @@
 import 'dotenv/config'
 import express from 'express'
-// import cors from 'cors'
 import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 // initialize app & modules
 const app = express()
-// app.use(cors())
 app.use(bodyParser.json())
 
-mongoose.connect(process.env.REMOTE_DB_URI, {
-  useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true
+// mongo connection
+const client = new MongoClient(process.env.REMOTE_DB_URI, { useNewUrlParser: true })
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('Titles');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs[0])
+    callback(docs);
+  });
+}
+
+client.connect(function (err) {
+  assert.equal(null, err)
+  console.log("Connected successfully to server")
+
+  const db = client.db('dev-challenge')
+  
+  findDocuments(db, function() {
+    client.close();
+  });
 })
-
-const db = mongoose.connection
-
-db.on('error', console.error.bind(console, 'Connection error:'))
-db.once('open', function () {
-  console.log("Mongoose connected to DB.")
-})
-
-// serve static dist build
 
 // api port listener
 const port = process.env.PORT || process.env.API_PORT
@@ -29,38 +41,6 @@ app.listen(port, () => {
   console.log("Express API server listening on Port:" + port)
 })
 
-// temp routes / controllers
 app.get('/', (req, res) => {
   res.send('Hello world!')
-})
-
-// temp schema
-const { Schema } = require('mongoose')
-const TitleSchema = new Schema(
-  {
-    Awards: Array,
-    Genres: Array,
-    KeyGenres: Array,
-    OtherNames: Array,
-    Participants: Array,
-    ReleaseYear: Number, // TODO: try casting as Int32 datatype ...?
-    Storylines: Array,
-    TitleId: String,
-    TitleName: String,
-    TitleNameSortable: String,
-    id: String
-  }
-)
-
-// temp model
-const Title = mongoose.model('Title', TitleSchema)
-
-// temp TitlesController
-app.get('/api/titles', (req, res) => {
-  let titleId = '534c60c9bc028401c08dc54e'
-  Title.findById({_id: titleId})
-    .then((title) => {
-      console.log("I AM TITLE:", title)
-      res.send("Hallo!")
-    })
 })
