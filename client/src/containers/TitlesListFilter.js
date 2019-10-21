dc0ba7710449e25fe3585f27436d3fb989311b5a
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-10-21 00:09:45 -0400

    Trying Filter component.

--- client/src/containers/TitlesListFilter.js
+++ client/src/containers/TitlesListFilter.js
@@ -2,26 +2,34 @@ import React, { Component } from 'react'
 import TitlesList from '../components/TitlesList'
 
 class TitlesListFilter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filtered: props.titles
-    }
+  state = {
+    initialTitles: [],
+    filteredTitles: []
   }
 
-  // componentDidMount() {
-  //   this.setState({
-  //     filtered: this.props.titles
-  //   });
-  // }
+  filterList = (event) => {
+    let titles = this.state.initialTitles;
+    titles = titles.filter((title) => {
+      return title.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
+    });
+    this.setState({ filteredTitles: titles });
+  }
 
+  componentDidMount() {
+    this.setState({
+      initinitialTitlesialItems: this.props.titles,
+      filteredTitles: this.props.titles
+    })
+    console.log(this.state)
+    console.log(this.props)
+  }
   render() {
     return (
       <div>
         <form>
-          <input type="text" className="input" placeholder="Search ..." />
+          <input type="text" placeholder="Search" onChange={this.filterList} />
         </form>
-        <TitlesList titles={this.state.filtered} />
+        <TitlesList titles={this.state.filteredTitles} />
       </div>
     )
   }
