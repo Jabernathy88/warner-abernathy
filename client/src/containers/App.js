9c0f1bd816dcf24c702abd30d691266fffc4b832
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-10-20 22:21:35 -0400

    Trying Context API.

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
+import axios from 'axios'
 import '../styles/app.css'
 
 function App() {
+  const [data, setData] = useState({ hits: [] })
+  const [query, setQuery] = useState('redux')
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        `http://hn.algolia.com/api/v1/search?query=${query}`
+        // `https://kusoft-pen-pal.herokuapp.com/api/v1/users/1`
+      );
+      setData(result.data)
+    };
+    fetchData()
+  }, [query])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Warner React App Sample</h2>
-      </header>
-    </div>
+    <Fragment>
+      <h2>Hello from Fragment App.js</h2>
+      <input
+        className="px-2"
+        type="text"
+        value={query}
+        onChage={event => setQuery(event.target.value)}
+      />
+      <button type="button" onClick={() => setSearch(query)}>
+        Search
+      </button>
+      <div className="pt-2">
+        {data.hits.map(item => (
+          <div key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </div>
+        ))}
+      </div>
+    </Fragment>
+
+
   )
 }
 
